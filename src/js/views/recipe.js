bb3d398f5f52f836ce1417f05b03d955a22c9952
AaaayushXD <lamichhane.aayush.al@gmail.com>
2022-11-01 14:43:49 +0545

    4th try

--- src/js/views/recipe.js
+++ src/js/views/recipe.js
@@ -1,6 +1,6 @@
 import icons from '../../img/icons.svg';
 import View from './view.js';
-import { Fraction } from 'fractional';
+import { Fraction } from '../../../node_modules/fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
